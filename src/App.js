cab2940b9d4d97677e2d6cd4c5aa2d12e641f117
jatin-pandey01 <=>
2023-09-24 22:21:45 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" exact element={<Feed/>}/>
+          <Route path="*" exact element={<Feed/>}/>
           <Route path="/video/:id" element={<VideoDetail/>} />
           <Route path="/channel/:id" element={<ChannelDetail/>} />
           <Route path="/search/:searchTerm" element={<SearchFeed/>} />
